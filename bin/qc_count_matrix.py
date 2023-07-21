086225f3da15c60c5fa9a835b3d01041ffa4de04
Rob Linder <rlinder02@gmail.com>
2023-07-20 17:04:29 -0700

    Generate QC metrics for the count matrix

---
+++ bin/qc_count_matrix.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python 
+# 4-space indented, v1.0.0
+# File name: qc_count_matrix.py
+# Description: This script can be used to calculate various QC metrics from scRNAseq data.
+# Author: Robert Linder
+# Date: 2023-07-20
+
+import argparse
+import pandas as pd
+import re
+from functools import wraps
+from seq_classes import Fasta
+
+
+
+
+
+
+
+
+
+
+def main():
+
+
+
+if __name__ == "__main__":
+	main()
